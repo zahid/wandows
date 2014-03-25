fcf17380aa6f8890e3215bb98dff2413a134c2ca
Zahid Mahir <zahid@zmhr.me>
2014-03-25 10:55:33 -0400

    added badii's source files

---
+++ src/Main.java
@@ -0,0 +1,71 @@
+//import java.io.*;
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) {
+		Scanner keyboard = new Scanner(System.in);
+		while (true) {
+			System.out.print("PROMPT\\>");
+			String command = keyboard.next();
+			String arguments = keyboard.nextLine();
+			Token token = new Token(command);
+			switch (token.kind) {
+			case Token.BIGGER:
+				break;
+			case Token.CAL:
+				new Cal(arguments);
+				break; // This is just a hint. You need to change the class Cal
+						// to displays the current month calendar.
+			case Token.CAT:
+				break;
+			case Token.ATTRIB:
+				break;
+			case Token.CP:
+				break;
+			case Token.CUT:
+				break;
+			case Token.D:
+				break;
+			case Token.ECHO:
+				break;
+			case Token.EXIT:
+				System.exit(0);
+				break;
+			case Token.FILE:
+				break;
+			case Token.FIND:
+				break;
+			case Token.GREP:
+				break;
+			case Token.HEAD:
+				break;
+			case Token.LINK:
+				break;
+			case Token.LS:
+				break;
+			case Token.DIR:
+				break;
+			case Token.MAN:
+				break;
+			case Token.MKDIR:
+				break;
+			case Token.MV:
+				break;
+			case Token.PWD:
+				break;
+			case Token.RM:
+				break;
+			case Token.RMDIR:
+				break;
+			case Token.TAIL:
+				break;
+			case Token.U:
+				break;
+			case Token.WC:
+				break;
+			default:
+				System.out.println("Wrong command.");
+			}
+		}
+	}
+}
---
+++ src/Token.java
@@ -0,0 +1,43 @@
+public class Token {
+	public int kind;
+
+	public Token(String spelling) {
+		kind = -1;
+		for (int k = BIGGER; k <= WC; k++)
+			if (spelling.equals(spellings[k])) {
+				this.kind = (byte) k;
+				break;
+			}
+	}
+
+	public final static byte BIGGER = 0, // >
+			CAL = 1, // cal
+			CAT = 2, // cat
+			ATTRIB = 3, // attrib
+			CP = 4, // cp
+			CUT = 5, // cut
+			D = 6, // d
+			ECHO = 7, // echo
+			EXIT = 8, // exit
+			FILE = 9, // file
+			FIND = 10, // find
+			GREP = 11, // grep
+			HEAD = 12, // head
+			LINK = 13, // link
+			LS = 14, // ls
+			DIR = 15, // dir
+			MAN = 16, // man
+			MKDIR = 17, // mkdir
+			MV = 18, // mv
+			PWD = 19, // pwd
+			RM = 20, // rm
+			RMDIR = 21, // rmdir
+			TAIL = 22, // tail
+			U = 23, // u
+			WC = 24; // wc
+
+	private final static String[] spellings = { ">", "cal", "cat", "attrib",
+			"cp", "cut", "d", "echo", "exit", "file", "find", "grep", "head",
+			"link", "ls", "dir", "man", "mkdir", "mv", "pwd", "rm", "rmdir",
+			"tail", "u", "wc" };
+}
