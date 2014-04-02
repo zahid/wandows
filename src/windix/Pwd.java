62f4c2332497ab92754ba18c69373f09f2fb377f
jshor <truthsofwolf@gmail.com>
2014-04-02 10:09:47 -0400

    removed text from println

--- src/windix/Pwd.java
+++ src/windix/Pwd.java
@@ -2,6 +2,6 @@ package windix;
 
 public class Pwd {
 	public static void main(String args[]) {
-		System.out.println("Current working directory: " + System.getProperty("user.dir"));
+		System.out.println(System.getProperty("user.dir"));
 	}
 }
