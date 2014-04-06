373d9dbe0c5619404a6b2b56945d0b9b863a822e
Zahid Mahir <zahid@zmhr.me>
2014-04-06 19:06:03 -0400

    removed uncessary files

--- src/commandows/Attrib.java
+++ src/commandows/Attrib.java
@@ -9,13 +9,11 @@ import java.nio.file.Paths;
 public class Attrib {
 	public static boolean[] flags = new boolean[4]; // -r=0, -a=1, -s=2, -h=3
 	public static String filename;
-	public static File file;
 	public static Path path;
 	
 	public Attrib(String[] args) throws IOException {
 		for(int i=0; i<args.length; i++) {
 			if(args[i].substring(0, 1).equals("-")) {
-				// the arg starts with "-" and is therefore a command. which command(s) is the user requesting?
 				switch(args[i].substring(1, 2)) {
 				case "r":
 					flags[0] = true;
@@ -35,7 +33,6 @@ public class Attrib {
 				filename = args[i];
 			}
 		}
-		file = new File(filename);
 		path = Paths.get(filename);
 		
 		if(flags[0]) {
@@ -77,7 +74,4 @@ public class Attrib {
 			System.err.println(e.getMessage());
 		}	
 	}
-	
-	
-	
 }
