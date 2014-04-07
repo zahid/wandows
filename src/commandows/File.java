aad44df2164341eacac813f9c7eb326b57f39583
Zahid Mahir <zahid@zmhr.me>
2014-04-06 20:31:13 -0400

    fixed file

--- src/commandows/File.java
+++ src/commandows/File.java
@@ -1,18 +1,13 @@
 package commandows;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class File {
 	public File(String[] args) throws IOException {
-		Path source = null;
+		int index;
 		try {
-			source = Paths.get(args[0]);
-			System.out.println(source.toAbsolutePath());
-			System.out.println(Files.exists(source));
-			System.out.println(Files.probeContentType(source.toAbsolutePath()));
+			index = args[0].indexOf(".");
+			System.out.println(args[0].substring(index + 1) + " file type");
 		} catch (Exception e) {
 			System.err.println(e.getMessage());
 		}
