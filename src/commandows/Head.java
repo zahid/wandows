30657844538c903715f4bdad8fed36e8172f80dd
Zahid Mahir <zahid@zmhr.me>
2014-04-06 22:05:45 -0400

    removed old imports

--- src/commandows/Head.java
+++ src/commandows/Head.java
@@ -1,9 +1,7 @@
 package commandows;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 
 public class Head {
 	BufferedReader in;
