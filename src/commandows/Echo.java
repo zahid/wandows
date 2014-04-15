aa3e23c282b5fc157abece99cd294b10abab647d
jshor <truthsofwolf@gmail.com>
2014-04-15 12:26:30 -0400

    ...

--- src/commandows/Echo.java
+++ src/commandows/Echo.java
@@ -3,11 +3,13 @@ package commandows;
 public class Echo {
 
 	public Echo(String[] args) {
-		try {
-			System.out.println(args[0]);
-		} catch (Exception e) {
-			System.err.println(e.getMessage());
-		}
+		String strOutput = "";
+		
+		if(args.length > 0)
+			for(int i=0; i<args.length; i++)
+				strOutput += args[i].trim() + " ";
+		
+		System.out.println(strOutput.trim());
 	}
 	
 }
