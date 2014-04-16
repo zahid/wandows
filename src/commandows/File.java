eae2f0f6fbed55b120ea9a0359f9f930c0a6aa41
jshor <truthsofwolf@gmail.com>
2014-04-15 22:38:57 -0400

    fixed flags

--- src/commandows/File.java
+++ src/commandows/File.java
@@ -2,12 +2,14 @@ package commandows;
 
 import java.io.IOException;
 
+import wandows.Main;
+
 public class File {
 	public File(String[] args) throws IOException {
 		int index;
 		try {
 			index = args[0].indexOf(".");
-			System.out.println(args[0].substring(index + 1) + " file type");
+			Main.out(args[0].substring(index + 1) + " file type");
 		} catch (Exception e) {
 			System.err.println(e.getMessage());
 		}
--- src/commandows/Wc.java
+++ src/commandows/Wc.java
@@ -10,7 +10,7 @@ public class Wc {
 	public static boolean[] flags = new boolean[3]; // -l=0, -m=1, -w=2
 	public static int[] sizes = new int[3]; // -l=0, -m=1, -w=2
 	
-	//wc -l -m -w C:/Users/Josh/Desktop/test2.txt C:/Users/Josh/Desktop/test.txt C:/Users/Josh/Desktop/tes3t.txt
+	//wc -l -m -w C:/Users/Josh/Desktop/test.txt C:/Users/Josh/Desktop/test.txt C:/Users/Josh/Desktop/tes3t.txt
 	
 	public Wc(String[] args) throws IOException {
 		for(int i=0; i<args.length; i++) {
@@ -38,10 +38,16 @@ public class Wc {
 		}
 		
 		// print counts for each file
-		for(int i=0; i<files.length; i++)
-			if(files[i] != null)
+		for(int i=0; i<files.length; i++) {
+			if(files[i] != null) {
+				// clear sizes first
+				sizes[0] = 0;
+				sizes[1] = 0;
+				sizes[2] = 0;
+				
 				readFile(files[i]);
-
+			}
+		}
 		// print totals
 		Main.outln("\t" + sizes[0] + "\t" + sizes[1] + "\t" + sizes[2] + "\ttotal");
 	}
@@ -61,36 +67,41 @@ public class Wc {
 				// read data into bytes array and then convert to String
 				raf.read(fileBytes);
 				String fileContents = new String(fileBytes);
+				int c1 = 0, c2 = 0, c3 = 0;
 				
 				// get delimiter count for each flag
 				if(flags[0]) {
-					int c = delimiterCount(fileContents, "\n");
-					sizes[0] += c;
-					System.out.print("\t" + c);
+					c1 = delimiterCount(fileContents, "\n");
+					sizes[0] += c1;
 				}
 				if(flags[1]) {
-					int c = delimiterCount(fileContents, "");
-					sizes[1] += c;
-					System.out.print("\t" + c);
+					c2 = delimiterCount(fileContents, "");
+					sizes[1] += c2;
 				}
 				if(flags[2]) {
-					int c = delimiterCount(fileContents, " ");
-					sizes[2] += c;
-					System.out.print("\t" + c);
+					c3 = delimiterCount(fileContents, " ");
+					c3 += delimiterCount(fileContents, "\n");
+					sizes[2] += c3;
 				}
 				
-				System.out.print("\t" + fileName + "\n");
+				Main.out("\n" + "\t" + c1 + "\t" + c2 + "\t" + c3 + "\t" + fileName);
 			} catch (IOException e) {
 				System.err.println("Cannot read file: " + e);
 			}
 		} else {
-			Main.outln("wc: " + fileName + ": no such file or directory.");
+			Main.outln("wc: `" + fileName + "`: no such file or directory.");
 		}
 	}
 	
 	public static int delimiterCount(String fileContents, String delimiter) {
 		String[] p = fileContents.split(delimiter);
+		int count = 0;
+		
+		for(int i=0; i<p.length; i++) {
+			if(p[i].replace("\n", "").replace("\t", "").replace(" ", "").length() > 0)
+				count++;
+		}
 
-		return p.length;
+		return count;
 	}
 }
