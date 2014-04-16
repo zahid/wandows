1caaadc0e549bab5b47d750d928b3b1f0d9fd54b
jshor <truthsofwolf@gmail.com>
2014-04-16 10:11:24 -0400

    fixed history index

--- src/commandows/Down.java
+++ src/commandows/Down.java
@@ -5,15 +5,14 @@ import wandows.Main;
 
 public class Down {
 	public Down() {
-		if(Main.commandHistory.size() >= Main.historyLevel && Main.historyLevel <= Main.commandHistory.size()) {
+		if(Main.commandHistory.size() >= Main.historyLevel && Main.historyLevel < Main.commandHistory.size()) {
+			Main.historyLevel++;
+			
 	    	// prevent caret controls while the process is executing
 			CaretListenerLabel.isRunning = true;
 	    	
 			String updatedText = Main.textHistory+ Main.commandHistory.get(Main.historyLevel-1);
 			Main.typingArea.setText(updatedText);
-			
-			if(Main.historyLevel-1 <= Main.commandHistory.size())
-				Main.historyLevel++;
 			
 			// prevent caret controls while the process is executing
 			CaretListenerLabel.isRunning = false;
--- src/commandows/Up.java
+++ src/commandows/Up.java
@@ -4,17 +4,16 @@ import wandows.*;
 public class Up {
 	public Up() {
 		if(Main.commandHistory.size() >= Main.historyLevel && Main.historyLevel > 0) {
-	    	// prevent caret controls while the process is executing
-			CaretListenerLabel.isRunning = true;
-	    	
-			String updatedText = Main.textHistory+ Main.commandHistory.get(Main.historyLevel-1);
-			Main.typingArea.setText(updatedText);
-			
-			if(Main.historyLevel-1 > 0)
 				Main.historyLevel--;
-			
-			// prevent caret controls while the process is executing
-			CaretListenerLabel.isRunning = false;
+				
+		    	// prevent caret controls while the process is executing
+				CaretListenerLabel.isRunning = true;
+		    	
+				String updatedText = Main.textHistory+ Main.commandHistory.get(Main.historyLevel);
+				Main.typingArea.setText(updatedText);
+				
+				// prevent caret controls while the process is executing
+				CaretListenerLabel.isRunning = false;
 		}
 	}
 }
